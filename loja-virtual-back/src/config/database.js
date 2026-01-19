6879bd7251806f0d19fc43ec1709a0c34fabc74c
davidgabriellm <davidglm.2002@gmail.com>
2026-01-19 16:24:55 -0300

    fix: config for neon postgres

--- loja-virtual-back/src/config/database.js
+++ loja-virtual-back/src/config/database.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
+  dialect: 'postgres', 
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
+
+  dialectOptions: {
+    ssl:true
+  }
+ 
 };
